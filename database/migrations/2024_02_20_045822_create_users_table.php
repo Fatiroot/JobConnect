990ce4587a8da53cf1b65197ee4738b33d265595
Rassili <adnan.rassili47@gmail.com>
2024-02-20 06:33:17 +0100

    add authentification and migration

--- database/migrations/2024_02_20_045822_create_users_table.php
+++ database/migrations/2024_02_20_045822_create_users_table.php
@@ -17,10 +17,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone',13);
+            $table->string('skill')->nullable();
+            $table->integer('status')->default(1);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 

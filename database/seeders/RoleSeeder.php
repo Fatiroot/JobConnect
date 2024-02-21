d5f81ceb9dd31d68164036505d8d6bf9d080472b
Rassili <adnan.rassili47@gmail.com>
2024-02-21 14:53:57 +0100

    add media library

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = [
+            [
+                'name' => 'admin',
+            ],
+            [
+                'name' => 'user',
+            ],
+            [
+                'name' => 'representative',
+            ],
+            [
+                'name' => 'Recruiter',
+            ]
+            ];
+            foreach ($roles as $role){
+                Role::create($role);
+            }
+    }
+}

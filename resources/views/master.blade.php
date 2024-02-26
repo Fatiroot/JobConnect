015e6ac42d0a57565cc9bfe4097bdbfdd3c7e80f
abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-02-26 09:23:31 +0100

    supprimer controller

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -34,10 +34,11 @@
             </button>
         </div>
         <nav class="text-white text-base font-semibold pt-3">
-            <a href="" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
+            <a href="{{route('dashboard')}}" class="flex items-center active-nav-link text-white py-4 pl-6 nav-item">
                 <i class="fas fa-tachometer-alt mr-3"></i>
                 Dashboard
             </a>
+            
             <a href="{{route('offer.index')}}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
                 <i class="fas fa-sticky-note mr-3"></i>
                 OFFRE D EMPLOIS
